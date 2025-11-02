27725f94b940d300b51271fb9203cb125551bd3d
Gyuray Kamber <gyuraykamber99@gmail.com>
2025-11-02 13:37:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8217158575-d6953",
+  "appId": "1:712179380083:web:b36156e4bcd3473b8a9f06",
+  "apiKey": "AIzaSyA8uUyXA_BQ-GyarKR0pujV9b0XW4gaWBc",
+  "authDomain": "studio-8217158575-d6953.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "712179380083"
+};
