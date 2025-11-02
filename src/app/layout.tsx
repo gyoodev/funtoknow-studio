77653f1d7abdb337c4a447fdc45b25b12784cd9f
Gyuray Kamber <gyuraykamber99@gmail.com>
2025-11-02 19:47:30 +0000

    npm i @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'FunToKnow Platform',
@@ -30,6 +31,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </FirebaseClientProvider>
+        <Analytics />
       </body>
     </html>
   );
